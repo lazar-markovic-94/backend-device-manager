2afb8476d26c7ca76da5faf6c2660b4907f767b1
lazar-markovic-94 <lazarmarkovic077@gmail.com>
2024-11-20 15:27:17 +0100

    Initial service setup and module creation.

---
+++ app/src/main/java/com/lazarmarkovic/app/BackendDeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lazarmarkovic.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeviceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDeviceManagerApplication.class, args);
+    }
+
+}
